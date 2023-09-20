24f0d1f2f77f57859d0f373671e22eb2c61167cd
darina9 <111124490+darina9@users.noreply.github.com>
2023-09-20 13:52:23 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+news = new Vue({
+    el: "#news",
+    data: {
+      cards: [
+        {
+          id: 1,
+          src: "img/blog1.png",
+          title: "Let’s Get Solution For Building Construction Work",
+          date: "26 December,2022 ",
+          buttonText: "Kitchan Design",
+        },
+        {
+          id: 2,
+          src: "img/blog2.png",
+          title: "Low Cost Latest Invented Interior Designing Ideas.",
+          date: "22 December,2022 ",
+          buttonText: "Living Design",
+        },
+        {
+          id: 3,
+          src: "img/blog3.png",
+          title: "Best For Any Office & Business Interior Solution",
+          date: "25 December,2022 ",
+          buttonText: "Interior Design",
+        },
+        {
+          id: 4,
+          src: "img/blog4.png",
+          title: "Let’s Get Solution For Building Construction Work",
+          date: "26 December,2022 ",
+          buttonText: "Kitchan Design",
+        },
+        {
+          id: 5,
+          src: "img/blog5.png",
+          title: "Low Cost Latest Invented Interior Designing Ideas.",
+          date: "22 December,2022 ",
+          buttonText: "Living Design",
+        },
+        {
+          id: 6,
+          src: "img/blog6.png",
+          title: "Best For Any Office & Business Interior Solution",
+          date: "25 December,2022 ",
+          buttonText: "Interior Design",
+        },
+      ],
+    },
+  });
